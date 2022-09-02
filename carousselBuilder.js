213f1007d13fde052e1b2802b950958510db6589
Thomas Cerqueira <thomas.cerqueira@epitech.eu>
2022-09-02 14:35:26 -0400

    [ADD] Icons, special text and color in caroussel

--- carousselBuilder.js
+++ carousselBuilder.js
@@ -1,12 +1,25 @@
 const websiteItems = [
     {
         img: "public/tree.jpg",
-        text: "Je suis un text de merde",
+        text: "Projet de []. Site web d'action reaction en relation avec des API. Découverte des [].",
+        special: [
+            "fin de 3ème année",
+            "webhooks"
+        ],
         title: "Area"
     },
     {
         img: "public/elephant.jpg",
-        text: "Un peux plus long pour voir ce que ça fait",
+        text: "Site web afin de donner des photos à des clients.\nConnexion custom avec [] afin que le client n'est qu'a rentré un UID.",
+        special: [
+            "FireBase"
+        ],
+        icons: [
+            {
+                url: "https://github.com/thomascerqueira/area-server",
+                icon: "fa-brands fa-github fa-2x"
+            }
+        ],
         title: "Elliot Web Site"
     },
     {
@@ -27,13 +40,44 @@ const template = $("#template-item").get(0)
 function addItems(item, index, where, totalLength) {
     const clone = document.importNode(template.content, true)
     const div = $("div", clone).get(0)
-    const span = $("span", clone).get(0)
+    const div_text = $(".ci-span", clone).get(0)
     const img = $("img", clone).get(0)
     const title = $("h2", clone).get(0)
+    const div_icons = $(".ci-icon", clone).get(0)
 
-    span.textContent = item.text
+
+    const texts = item.text.split('[]')
+    const icons = item.icons
+    const special = item.special
+
+    for (const i in texts) {
+        const spanD = document.createElement("span")
+        spanD.textContent = texts[i]
+
+        div_text.appendChild(spanD)
+
+        if (special && special[i]) {
+            const spanS = document.createElement("span")
+            spanS.textContent = special[i]
+            spanS.style.fontWeight = 800
+            spanS.style.color = "hsla(var(--red), 0.8)"
+            div_text.appendChild(spanS)
+        }
+    }
+    
     img.src = item.img
     title.textContent = item.title
+    
+    for (const i in icons) {
+        const icon = icons[i]
+        const iE = document.createElement("i")
+        iE.classList = [icon.icon]
+        iE.onclick = function () {
+            window.open(icon.url)
+        } 
+
+        div_icons.appendChild(iE)
+    }
 
     if (index === 0) {
         div.classList.add("active")
