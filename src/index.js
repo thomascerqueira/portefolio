3c2643c76c14c8dd8f647c30800a3cffa654e597
Cerqueira Thomas <thomas.cerqueira@epitech.eu>
2023-02-15 15:48:16 -0500

    feat(V2): animation on hover et button back

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const nameContainer = document.getElementById("name")
-const divs = nameContainer.querySelectorAll("div")
 const T = document.getElementById("T")
 const H = document.getElementById("H")
 const O = document.getElementById("O")
@@ -7,8 +6,23 @@ const M = document.getElementById("M")
 const A = document.getElementById("A")
 const S = document.getElementById("S")
 
-function addOutside() {
+const divs = [T, H, O, M, A, S]
 
+const clickContainer = document.getElementById("click-container")
+
+const buttonBackContainer = document.getElementById("back-button-container")
+const buttonBack = document.getElementById("back-button")
+
+let where = "home"
+
+function addOutside() {
+    // buttonBackContainer.style.display = "flex"
+    buttonBackContainer.style.opacity = "1"
+    buttonBack.classList.remove("hide")
+
+    clickContainer.style.display = "none"
+
+    where = "amo"
     T.classList.add("outside")
     H.classList.add("outside")
     S.classList.add("outside")
@@ -21,6 +35,13 @@ function addOutside() {
 }
 
 function removeOutside() {
+    // buttonBackContainer.style.display = "none"
+    buttonBackContainer.style.opacity = "0"
+    buttonBack.classList.add("hide")
+    clickContainer.style.display = "flex"
+
+    where = "Home"
+
     divs.forEach(div => div.style.width = "var(--size)")
     T.classList.remove("outside")
     H.classList.remove("outside")
@@ -31,10 +52,14 @@ function removeOutside() {
     A.classList.remove("after")
 }
 
+
+
 nameContainer.addEventListener("click", () => {
-    if (T.classList.contains("outside")) {
-        removeOutside()
-    } else {
+    if (!T.classList.contains("outside")) {
         addOutside()
     }
+})
+
+buttonBack.addEventListener("click", () => {
+    removeOutside()
 })
